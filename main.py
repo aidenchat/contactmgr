e8540d2b3c2086acacc003c3a1aa70639c66197d
el <144085979+traaashday@users.noreply.github.com>
2023-11-27 19:58:39 +0800

    added various save files ;)

--- main.py
+++ main.py
@@ -27,32 +27,100 @@ def add_contact(contacts):
     contacts["contacts"].append(new_contact)
     print("Contact added successfully.")
 
-def update_contact(contacts): #should can search by any category?
-    name = input("Enter the name of the contact to update: ")
+def update_contact(contacts):
+    search_category = input("Enter the category to search in (name, phone, email, tags): ")
+    search_term = input("Enter the search term: ")
 
     for contact in contacts["contacts"]:
-        name_pattern = re.compile(name, re.IGNORECASE)
+        if search_category == "name":
+            search_pattern = re.compile(search_term, re.IGNORECASE)
+            if search_pattern.search(contact["name"]):
+                option = input("Merge(m) or remove (r)? ")
+                if option.lower() == "r":
+                    contacts["contacts"].remove(contact)
+                elif option.lower() == "m":
+                    phone = input("Enter the new phone number (leave blank to keep it unchanged): ")
+                    email = input("Enter the new email address (leave blank to keep it unchanged): ")
+                    tags = input("Enter the new tags (comma-separated, leave blank to keep them unchanged): ")
 
-        if name_pattern.search(contact["name"]) :
-            option= input("Merge(m) or remove (r)? ") #both merge and r still sucks: can't merge phone no, can't remove
-            if option.lower()== "r":
-                    if contact["name"].lower() == name.lower():
-                        del contacts["contacts"]
-            elif option.lower()== "m":
-                phone = input("Enter the new phone number (leave blank to keep it unchanged): ")
-                email = input("Enter the new email address (leave blank to keep it unchanged): ")
-                tags = input("Enter the new tags (comma-separated, leave blank to keep them unchanged): ")
+                    if phone:
+                        contact["phone"] = phone
+                    if email:
+                        contact["email"] = email
+                    if tags:
+                        new_tags = tags.split(',')
+                        contact["tags"].extend(new_tags)
 
-                if phone:
-                    contact["phone"] = phone
-                if email:
-                    contact["email"] = email
-                if tags:
-                    new_tags = tags.split(',')
-                    contact["tags"].extend(new_tags)
+                    print("Contact updated successfully.")
+                    return contact
 
-                print("Contact updated successfully.")
-                return contact
+        elif search_category == "phone":
+            search_pattern = re.compile(search_term, re.IGNORECASE)
+            if search_pattern.search(contact["phone"]):
+                option = input("Merge(m) or remove (r)? ")
+                if option.lower() == "r":
+                    contacts["contacts"].remove(contact)
+                elif option.lower() == "m":
+                    name = input("Enter the new name (leave blank to keep it unchanged): ")
+                    email = input("Enter the new email address (leave blank to keep it unchanged): ")
+                    tags = input("Enter the new tags (comma-separated, leave blank to keep them unchanged): ")
+
+                    if name:
+                        contact["name"] = name
+                    if email:
+                        contact["email"] = email
+                    if tags:
+                        new_tags = tags.split(',')
+                        contact["tags"].extend(new_tags)
+
+                    print("Contact updated successfully.")
+                    return contact
+
+        elif search_category == "email":
+            search_pattern = re.compile(search_term, re.IGNORECASE)
+            if search_pattern.search(contact["email"]):
+                option = input("Merge(m) or remove (r)? ")
+                if option.lower() == "r":
+                    contacts["contacts"].remove(contact)
+                elif option.lower() == "m":
+                    name = input("Enter the new name (leave blank to keep it unchanged): ")
+                    phone = input("Enter the new phone number (leave blank to keep it unchanged): ")
+                    tags = input("Enter the new tags (comma-separated, leave blank to keep them unchanged): ")
+
+                    if name:
+                        contact["name"] = name
+                    if phone:
+                        contact["phone"] = phone
+                    if tags:
+                        new_tags = tags.split(',')
+                        contact["tags"].extend(new_tags)
+
+                    print("Contact updated successfully.")
+                    return contact
+
+        elif search_category == "tags":
+            search_pattern = re.compile(search_term, re.IGNORECASE)
+            if any(search_pattern.search(tag) for tag in contact["tags"]):
+                option = input("Merge(m) or remove (r)? ")
+                if option.lower() == "r":
+                    contacts["contacts"].remove(contact)
+                elif option.lower() == "m":
+                    name = input("Enter the new name (leave blank to keep it unchanged): ")
+                    phone = input("Enter the new phone number (leave blank to keep it unchanged): ")
+                    email = input("Enter the new email address (leave blank to keep it unchanged): ")
+
+                    if name:
+                        contact["name"] = name
+                    if phone:
+                        contact["phone"] = phone
+                    if email:
+                        contact["email"] = email
+
+                    print("Contact updated successfully.")
+                    return contact
+
+    print("Contact not found.")
+    return None
                 
 def filter_contacts_by_tag(contacts, tag):
     filtered_contacts = []
@@ -109,8 +177,18 @@ if __name__ == "__main__":
             filter_contacts_by_tag(contacts, tag)
         elif choice.lower() == "add":
             add_contact(contacts)
+            save_contacts(file_path, contacts)
         elif choice.lower() == "update":
             update_contact(contacts)
+            save_contacts(file_path, contacts)
+            contacts = load_contacts(file_path)
+            updated_contact = update_contact(contacts)
+            if updated_contact:
+                print("Updated Contact:")
+                print("Name:", updated_contact["name"])
+                print("Phone:", updated_contact["phone"])
+                print("Email:", updated_contact["email"])
+                print("Tags:", ', '.join(updated_contact["tags"]))
         elif choice.lower() == "search":
             sterm = input("Input search term: ")
             search_results = fuzzy_search(contacts, sterm)
